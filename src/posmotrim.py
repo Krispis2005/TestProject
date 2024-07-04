36fd169874c60335a69710feca3c59e3eb1be902
Krispis <stepanovaks05@mail.ru>
2024-07-04 13:11:58 +0500

    posmotrim

--- src/posmotrim.py
+++ src/posmotrim.py
@@ -1,12 +1,11 @@
-print ("Введите количество игроков")
+print("Введите количество игроков")
 N = int(input())
-print ("Хорошо, вас", N,", а теперь назовитесь")
-min_name = "я"
+print("Хорошо, вас", N,", а теперь назовитесь")
+min = "яяяяяяя"
 for i in range(N):
     name = input()
-    if min_name == "я":
-        min_name = name
-    elif name < min_name:
-          min_name = name
-          print (min_name)
+    if name < min:
+        min = name
+
+print(min)
 
